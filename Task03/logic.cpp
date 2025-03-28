eda36a89bf8d614b7996beb11d92966de3528690
Fidachyo322 <baconvevo@mail.ru>
2025-03-28 19:45:32 +0300

    не доделал task03

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -2,11 +2,44 @@
 // [The arithmetic mean of non - extreme elements]
 //
 // Дан вектор натуральных чисел. Необходимо разработать 
-// функцию, которая вычисляет среднее рифметическое элементов
+// функцию, которая вычисляет среднее aрифметическое элементов
 // вектора без учета минимального и максимального элементов.
 
 #include "logic.h"
 
+int get_min(int* array, int count) {
+	int min = array[0];
+	for (int i = 0; i < count; i++)
+	{
+		if (array[i] < min) {
+			min = array[i];
+		}
+	}
+}
+
+
+int get_max(int* array, int count) {
+	int max = array[0];
+	for (int i = 0; i < count; i++)
+	{
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+}
+
 double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size) {
-	return 0.0;
+
+	double sum = 0;
+	int count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > get_min(array, count) && array[i] <= get_max(array, count))
+		{
+			sum += array[i];
+		}
+	}
+
+	return sum / count;
 }
--- Task03/logic.h
+++ Task03/logic.h
@@ -1,1 +1,3 @@
 double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size);
+int get_min(int* array, int count);
+int get_max(int* array, int count);
